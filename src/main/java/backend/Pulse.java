df7dcd704e8ec48a7e6cd55f2c53042c8b8886d8
Eric <erfan.raygan@adelaide.edu.au>
2016-07-30 12:14:59 +0930

    Pojo setup

---
+++ src/main/java/backend/Pulse.java
@@ -0,0 +1,62 @@
+package backend;
+
+public class Pulse {
+
+  public static enum Sentiment {
+    AGGREVATED, AVERAGE, HAPPY
+  }
+
+  public static enum Heart {
+    BUSINESS, EVENT
+  }
+
+  private int intensity;
+  private Sentiment sentiment;
+  private LatLong location;
+  private Heart heart;
+
+  public Pulse() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public Pulse(int intensity, Sentiment sentiment, LatLong location, Heart heart) {
+    super();
+    this.intensity = intensity;
+    this.sentiment = sentiment;
+    this.location = location;
+    this.heart = heart;
+  }
+
+  public int getIntensity() {
+    return intensity;
+  }
+
+  public void setIntensity(int intensity) {
+    this.intensity = intensity;
+  }
+
+  public Sentiment getSentiment() {
+    return sentiment;
+  }
+
+  public void setSentiment(Sentiment sentiment) {
+    this.sentiment = sentiment;
+  }
+
+  public LatLong getLocation() {
+    return location;
+  }
+
+  public void setLocation(LatLong location) {
+    this.location = location;
+  }
+
+  public Heart getHeart() {
+    return heart;
+  }
+
+  public void setHeart(Heart heart) {
+    this.heart = heart;
+  }
+
+}
