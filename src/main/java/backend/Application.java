c5ae5440453b6891aed78d7cbcb4d08529dff68e
Eric <erfan.raygan@adelaide.edu.au>
2016-07-30 09:45:56 +0930

    FINALLY SUCCESS IN BUILD

--- src/main/java/backend/Application.java
+++ src/main/java/backend/Application.java
@@ -1,9 +1,54 @@
 package backend;
 
+import static spark.Spark.*;
+
+import spark.Spark;
+
 public class Application {
 
-  public Application() {
-    
+  public static void main(String[] args) {
+    Spark.port(8080);
+    Spark.threadPool(10);
+
+    registerStaticTestRoutes();
+
+    registerBeforeFilters();
+    registerGetRoutes();
+    registerPostRoutes();
+    registerPutRoutes();
+    registerDeleteRoutes();
+  }
+
+  public static void registerDeleteRoutes() {
+    Spark.get("/test", (request, response) -> {
+      return "TESTING COMPLETED";
+    });
+
+  }
+
+  public static void registerPutRoutes() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public static void registerPostRoutes() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public static void registerGetRoutes() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public static void registerBeforeFilters() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public static void registerStaticTestRoutes() {
+    // TODO Auto-generated method stub
+
   }
 
 }
